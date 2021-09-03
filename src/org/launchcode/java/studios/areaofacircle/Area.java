28a3c364841fbe339bdcc521494e53ca17ae060b
Anastasia Okuonghae <aokuonghae@gmail.com>
2021-09-02 21:46:34 -0500

    added some commentary for students"

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,7 +1,9 @@
 package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
+//You can totally have classes within this main file. Or make a seperate class e.g Circle
 class Check {
+    //creating a function to test strings
     public static boolean isNumeric(String string) {
         int intValue;
 
@@ -9,6 +11,10 @@ class Check {
             return false;
         }
 
+        /*This is a try catch block. Try to conver string to an integer and
+        if it works, return true thus ending the function.
+        Else, it will catch the error and print out.
+         */
         try {
             intValue = Integer.parseInt(string);
             return true;
@@ -24,15 +30,18 @@ public class Area {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a radius: ");
+        //save the input as a string
         String value = in.nextLine();
+        //check the string
         if (Check.isNumeric(value)) {
+            //now the string is converted to a Double
             double radius = Double.parseDouble(value);
             while (radius < 0) {
                 System.out.println("I only work in normal space. Provide a positive radius.");
                 radius = in.nextDouble();
             }
             //Note that once you are in the while loop, the user could throw in a string and it wouldn't
-            //catch it. If you've got the time, you can challenge yourself to try to catch that.
+            //catch it. If you've got the time, you can challenge yourself to try to counter that.
             Double areaOfCircle = Circle.getArea(radius);
             System.out.println("The area of a circle of radius " + radius + " is: " + areaOfCircle);
         } else {
